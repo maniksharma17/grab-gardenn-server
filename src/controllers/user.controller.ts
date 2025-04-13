9b4fc83e58eb5d4c5da799e01bf5254d6bbb57f9
Manik Sharma <maniksharma.rke@gmail.com>
2025-04-14 01:58:36 +0530

    new

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -68,7 +68,7 @@ export const login = async (req: Request, res: Response) => {
 
     res.json({ user: { ...user.toObject(), password: undefined }, token });
   } catch (error) {
-    throw error;
+    return res.status(500).json({ message: 'Some server error occured' });
   }
 };
 
