ee45584c78b0237e7665c97297d7f19f500fec57
Manik Sharma <maniksharma.rke@gmail.com>
2025-05-27 16:28:07 +0530

    ne

--- dist/schemas/category.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.categorySchema = void 0;
-const zod_1 = require("zod");
-exports.categorySchema = zod_1.z.object({
-    name: zod_1.z.string().min(2),
-    description: zod_1.z.string().min(10),
-    image: (zod_1.z.string().url()).optional(),
-});
