f7b6938ceb32c38fec3235032c264ba1223b3402
Manik Sharma <maniksharma.rke@gmail.com>
2025-05-29 02:15:39 +0530

    final

--- src/schemas/product.schema.ts
+++ src/schemas/product.schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const productSchema = z.object({
   name: z.string().min(2),
-  hindiName: z.string().min(2).optional(),
+  hindiName: z.string().optional(),
   description: z.string().min(10),
   price: z.array(z.number().positive()),
   cutoffPrice: z.array(z.number().positive()),
@@ -19,7 +19,7 @@ export const productSchema = z.object({
   category: z.string(),
   benefits: z.array(z.string()),
   ingredients: z.array(z.string()),
-  storage: z.string().min(5),
+  storage: z.string().optional(),
   instructions: z.array(z.string()),
   stock: z.number(),
 });
